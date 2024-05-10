7c8f87b9c0b51896d3f3d1495d018736d55f5e43
Степан Бокарев <bokarevstepan1@yandex.ru>
2024-05-10 20:46:18 +0300

    ADD

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,10 +3,13 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* Старт приложения здесь
+* Степан Бокарев
+* */
 @SpringBootApplication
 public class FilmorateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
-
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,32 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Film {
+    private Long id;
+
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Описание не может быть пустым")
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов.")
+    private String description;
+
+    @NotNull(message = "Release date cannot be null")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Duration must be greater than zero")
+    private int duration;
 }
