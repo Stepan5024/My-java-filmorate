8a73f9155fcc79f814d66cc2e4d11e1281c6730d
Степан <bokarevstepan1@yandex.ru>
2024-06-19 16:30:30 +0300

    delete unused import

--- src/main/java/ru/yandex/practicum/filmorate/service/genre/IGenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/genre/IGenreService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service.genre;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IGenreService {
     Genre findById(Long id);
